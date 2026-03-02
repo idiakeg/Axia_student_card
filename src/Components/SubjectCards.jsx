5d31754f105b7a043339e8c3ba5333ab25d29ed2
idiakeg <idiakeg@gmail.com>
2026-03-02 23:40:49 +0100

    first iteration

---
+++ src/Components/SubjectCards.jsx
@@ -0,0 +1,25 @@
+import SubjectCard from "./SubjectCard";
+import "../styles/SubjectCard.css";
+
+const SubjectCards = ({ data }) => {
+    return (
+        <>
+            {data.length > 1 ? (
+                <div className="subject_card_wrapper">
+                    {data.map(({ id, subject, grade, teacher }) => (
+                        <SubjectCard
+                            key={id}
+                            subject={subject}
+                            grade={grade}
+                            teacher={teacher}
+                        />
+                    ))}
+                </div>
+            ) : (
+                <div className="no_subj_found">No subject found.</div>
+            )}
+        </>
+    );
+};
+
+export default SubjectCards;
